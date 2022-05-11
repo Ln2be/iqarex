1293b23364df8bd15bd32ae9cd59242c77605db0
hacenselahy@gmail.com <hacenselahy@gmail.com>
2022-05-11 13:36:57 +0000

    increase quality images

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,17 @@ router.get("/posts", async function (req, res, next) {
   res.json(posts);
 });
 
+// router.get("/deleteall", async function (req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   const posts = await models.DBPost.deleteMany({});
+//   res.json(posts);
+// });
+
+router.get("/delete", async function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  const { id } = req.query;
+  const posts = await models.DBPost.deleteOne({ _id: id });
+  res.json(posts);
+});
+
 module.exports = router;
